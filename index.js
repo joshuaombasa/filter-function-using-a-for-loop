4cec5d08d7cf23fa50d905de2f3bd9fa3a8d09d0
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-04-07 22:20:49 +0300

    upadted script

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const people = [
 function getPetOwners(array, callback) {
       const petOwners = []
       for (let i=0; i<people.length; i++) {
-        
+        const item = callback()
       }
 }
 
